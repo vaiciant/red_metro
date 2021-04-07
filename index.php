5565daafe133445735a2f7e3dd140b1ffb83e7df
vaiciant <32634723+vaiciant@users.noreply.github.com>
2021-04-07 00:19:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 //El archivo .red es un archivo de texto con extension [nombre_archivo].red
 //El formato del archivo .red es el siguiente
-//[nombre_parada]|[color_parada]|[{nombres_paradas_anteriores| ...} = null]
+//[nombre_parada]|[color_parada]|[{nombres_paradas_anteriores| ...} = null],[nombre_parada]|[color_parada]|[{nombres_paradas_anteriores|...} = null],...
 //Los colores de las paradas son los siguientes:
 //0 = sin color
 //1 = verde
@@ -23,96 +23,12 @@
 
 //Se crean las clases en php por la subida de archivos y despues se crea las clases en javascript, con los datos de php
 
+//Se importan las clases en php
+require 'clases/clases.php';
+
 //Inicializar variables de red y subida
 $red = null;
 $uploadOk = 0;
-
-//Clase Parada, contiene los datos de cada parada
-class Parada {
-    //Nombre de la parada, se crea mediante el primer dato del archivo .red
-    public $nombre = '';
-    //Color de la parada, se crea mediante el segundo dato del archivo .red
-    //0 = sin color
-    //1 = verde
-    //2 = rojo
-    public $color = 0;
-    //Array de paradas anteriores, se crea retroactivamente
-    public $anterior = array();
-    //Array de paradas siguientes, se crea mediante el tercer y consecutivos datos del archivo .red
-    public $siguiente = array();
-
-    //Muestra la parada con su color, en html
-    function mostrar() {
-        $colorMostrar = "<parada>";
-        if ($this->color == 1) {
-            $colorMostrar = '<parada style="color: green">';
-        } else if ($this->color == 2) {
-            $colorMostrar = '<parada style="color: red">';
-        }
-        return $colorMostrar.$this->nombre.'</parada>';
-    }
-}
-
-//Clase Red, contiene las paradas de la red del metro
-class Red {
-    //Nombre de la red, se da por el nombre del archivo
-    public $nombre = "";
-    //Array de paradas de la red
-    public $paradas = array();
-
-    //Devuelve una clase Parada si coincide con el nombre dado
-    function buscarNombre(string $nombreBuscar) {
-        foreach($this->paradas as $parada) {
-            if ($parada->nombre == $nombreBuscar) {
-                return $parada;
-            }
-        }
-        return null;
-    }
-
-    //Funcion para ingresar paradas por el archivo .red
-    function ingresarParadas(string $cadena) {
-        //Remplaza los espacios y saltos de lines del archivo ingresado
-        $cadena = str_replace(array("\r", "\n", " "), '', $cadena);
-
-        //Divide en un array por las comas la cadena de texto del archivo que se ingresa
-        $paradas = explode(",", $cadena);
-
-        //Loop con todas las paras
-        foreach ($paradas as $parada) {
-            //Divide en un array por los datos de cada parada
-            $datos = explode("|", $parada);
-            $paradaClass = new Parada();
-            $countDatos = 0;
-            //Crea e ingresa las paradas a la red con sus anteriores paradas
-            foreach ($datos as $dato) {
-                if ($countDatos == 0) {
-                    $paradaClass->nombre = $datos[0];
-                } else if ($countDatos == 1) {
-                    if ($dato >= 0 and $dato < 3) {
-                        $paradaClass->color = $dato;
-                    } else {
-                        $paradaClass->color = 0;
-                    }
-                } else {
-                    $paradaClass->anterior[] = $dato;
-                }
-                $countDatos++;
-            }
-            $this->paradas[] = $paradaClass;
-        }
-
-        //Ingresa las paradas siguientes de las paradas ya creadas
-        foreach ($this->paradas as $parada) {
-            foreach ($parada->anterior as $nombreParadaAnterior) {
-                $paradaBuscada = $this->buscarNombre($nombreParadaAnterior);
-                if ($paradaBuscada != null) {
-                    $paradaBuscada->siguiente[] = $parada->nombre;
-                }
-            }
-        }
-    }
-}
 
 //Este metodo se activa al subir el archivo .red
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -125,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $red = new Red();
 
         //Se guarda el nombre de la red por el nombre del archivo
-        $red->nombre = substr(strtolower($_FILES["archivoRed"]["name"]), 0, -4);
+        $red->nombre = substr($_FILES["archivoRed"]["name"], 0, -4);
 
         //Ingresar paradas
         $red->ingresarParadas($cadena);
@@ -141,10 +57,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
     <head>
         <!-- estilo de la pagina -->
-        <link rel="stylesheet" href="estilo.css">
+        <link rel="stylesheet" href="css/estilo.css">
     </head>
-    <body style="margin-left: 50px;">
+    <body style="margin-left: 50px; margin-right: 50px;">
 
+    <!-- Ver pruebas unitarias -->
+    <a href="/pruebas.php">Pruebas Unitarias</a><br>
     <!-- Inicio subida archivo .red -->
     <a href="/ejemplo.red" download>Descargar archivo .red de ejemplo</a><br>
     <br>
@@ -275,8 +193,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     </body>
     <!-- Las clases se deben crear de nuevo para que funciones en javascript, se tendran que crear nuevas funciones para la logica del trayecto -->
-    <!-- Se carga la arquitecura de las clases -->
-    <script src="clases.js"></script>
+    <!-- Se importan las clases de javascript -->
+    <script src="clases/clases.js"></script>
     <script>
         //Inicializacion de las paradas ingresadas por php
         var paradas = new Array();
@@ -310,18 +228,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             ?>
             //Inicializacion de la red por las paradas ingresadas por php
             red = new Red("<?php echo $red->nombre ?>", paradas);
-            for (var paradaRootSig of red.paradas) {
-                if (paradaRootSig.anterior.length == 0) {
-                    red.crearMapasSiguienteRecursivo(paradaRootSig);
-                    red.trayectoTemp = "";
-                }
-            }
-            for (var paradaRootAnt of red.paradas) {
-                if (paradaRootAnt.siguiente.length == 0) {
-                    red.crearMapasAnteriorRecursivo(paradaRootAnt);
-                    red.trayectoTemp = "";
-                }
-            }
+            red.crearMapaRecursivo();
         <?php
         }
         ?>
@@ -342,8 +249,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <?php
             if ($red != null) {
                 ?>
-                red.trayectosReales = new Array();
-
                 //Asigna las pardas de incio y de fin
                 var primeraParada = red.paradas[selectIni.value];
                 var ultimaParada = red.paradas[selectFin.value];
@@ -368,17 +273,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     //Si la primera y ultima parada es correcta, se avanza en el calculo
                     if (resultado){
                         //Se busca el trayecto siguiente real
-                        red.buscarTrayectoSiguiente(primeraParada, ultimaParada, trenColor.value);
-
-                        //Si no se encuentra un trayecto siguiente, se busca en los anteriores
-                        var anterior = false;
-                        if (red.trayectosReales.length == 0) {
-                            anterior = true;
-                            red.buscarTrayectoAnterior(primeraParada, ultimaParada, trenColor.value);
-                        }
+                        trayectosExiste = red.buscarTrayectos(primeraParada, ultimaParada, trenColor.value);
 
                         //Si existe un trayecto estos se muestran
-                        if (red.trayectosReales.length > 0) {
+                        if (trayectosExiste) {
                             //Se busca el trayecto con menor paradas
                             for (var trayeco of red.trayectosReales) {
                                 if (trayectoMenor.length > trayeco.length) {
@@ -502,6 +400,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     for (var trayecto of red.trayectosAnteriores) {
                         html += "&nbsp;&nbsp;&nbsp;&nbsp;" + trayecto + "<br>";
                     }
+
 
                     //Se escribe el resultado en la vista del debug
                     var debugResultado = document.getElementById('debug');
